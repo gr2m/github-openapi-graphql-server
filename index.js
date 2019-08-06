441d940268efd6466c96dbb797f8cd9a8f0b0a38
Gregor Martynus <gregor@martynus.net>
2019-08-06 14:55:57 -0700

    feat: endpoint(route) query

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const { readFileSync } = require("fs");
 
 const { GraphQLServer } = require("graphql-yoga");
 const getEndpoints = require("./lib/get-endpoints");
+const getEndpoint = require("./lib/get-endpoint");
 const getReleases = require("./lib/get-releases");
 
 const typeDefs = readFileSync("./schema.graphql", "utf8");
 
 async function main({ token }) {
-  const state = { token };
+  const state = {
+    token,
+    endpoints: {}
+  };
 
   const resolvers = {
     Query: {
       endpoints: async (_, options) => getEndpoints(state, options),
+      endpoint: async (_, options) => getEndpoint(state, options),
       releases: async () => getReleases(state),
       lastRelease: async () => {
         const [release] = await getReleases(state);
