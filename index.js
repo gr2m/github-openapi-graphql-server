0db9d95353c4217889734712c91219bcee7189a0
Gregor Martynus <gregor@martynus.net>
2019-09-10 22:49:39 -0700

    feat: lots of fixes and additions
    From working on upgrading @octokit/rest using the GraphQL server

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ module.exports = main;
 const { readFileSync } = require("fs");
 
 const { GraphQLServer } = require("graphql-yoga");
+
 const getEndpoints = require("./lib/get-endpoints");
 const getEndpoint = require("./lib/get-endpoint");
 const getReleases = require("./lib/get-releases");
+const formatStringResolver = require("./lib/format-string-resolver");
 
 const typeDefs = readFileSync("./schema.graphql", "utf8");
 
@@ -24,9 +26,17 @@ async function main({ token }) {
         const [release] = await getReleases(state);
         return release;
       }
+    },
+    Endpoint: {
+      id: formatStringResolver.bind(null, 'id'),
+      scope: formatStringResolver.bind(null, 'scope')
+    },
+    Rename: {
+      before: formatStringResolver.bind(null, 'before'), 
+      after: formatStringResolver.bind(null, 'after')
     }
   };
 
   const server = new GraphQLServer({ typeDefs, resolvers });
-  server.start(() => console.log("Server is running on localhost:4000"));
+  server.start(() => console.log("Server is running on http://localhost:4000"));
 }
