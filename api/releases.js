10a8d803012350048b6f499583ed682b7df8aa20
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-11-04 09:45:41 -0800

    fix: better error handling

--- api/releases.js
+++ api/releases.js
@@ -11,27 +11,34 @@ const semver = require("semver");
  * @param {import("@vercel/node").NowResponse} res
  */
 module.exports = async (req, res) => {
-  const { body } = await got(
-    "https://api.github.com/repos/github/rest-api-description/releases?per_page=100",
-    {
-      headers: process.env.GITHUB_TOKEN
-        ? {
-            authorization: `token ${process.env.GITHUB_TOKEN}`,
-          }
-        : {},
-    }
-  );
-  const data = JSON.parse(body);
-  const versions = data
-    .map((release) => {
-      return {
-        version: release.tag_name.substr(1),
-        created_at: release.created_at,
-      };
-    })
-    .filter((release) => semver.valid(release.version))
-    .sort((a, b) => semver.compare(a.version, b.version));
+  try {
+    const { body } = await got(
+      "https://api.github.com/repos/github/rest-api-description/releases?per_page=100",
+      {
+        headers: process.env.GITHUB_TOKEN
+          ? {
+              authorization: `token ${process.env.GITHUB_TOKEN}`,
+            }
+          : {},
+      }
+    );
+    const data = JSON.parse(body);
+    const versions = data
+      .map((release) => {
+        return {
+          version: release.tag_name.substr(1),
+          created_at: release.created_at,
+        };
+      })
+      .filter((release) => semver.valid(release.version))
+      .sort((a, b) => semver.compare(a.version, b.version));
 
-  res.setHeader("cache-control", "s-maxage=180");
-  res.json(versions);
+    res.setHeader("cache-control", "s-maxage=180");
+    res.json(versions);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500);
+    res.json({ error });
+  }
 };
